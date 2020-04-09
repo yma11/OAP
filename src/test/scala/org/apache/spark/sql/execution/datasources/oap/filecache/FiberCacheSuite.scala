36f4982563999e81e75ac841be126eef62a58e27
yan ma <yan.ma@intel.com>
2020-04-09 11:06:47 +0800

    correct ut name to fibercachesuite

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/FiberCacheSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/FiberCacheSuite.scala
@@ -30,7 +30,7 @@ import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.{ByteBufferOutputStream, Utils}
 
-class MemoryManagerSuite extends SharedOapContext {
+class FiberCacheSuite extends SharedOapContext {
 
   private var random: Random = _
   private var values: Seq[Any] = _
