8b7749c277f477d0c46b27859c945799d38cc77b
Ferdinand Xu <cheng.a.xu@intel.com>
2020-05-14 09:51:07 +0800

    Implement FileChunk (#1283)

--- oap-common/src/main/java/com/intel/oap/common/storage/FileChunk.java
+++ oap-common/src/main/java/com/intel/oap/common/storage/FileChunk.java
@@ -1,13 +1,58 @@
 package com.intel.oap.common.storage;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileChunk implements Chunk {
-    @Override
-    public void writeDataToStore(Object baseObj, long baseAddress, long offset) {
-        // TODO fileStream write to data
+    private FileInputStream fileInputStream;
+    private FileOutputStream fileOutputStream;
+    private byte [] id;
+
+    public FileChunk(byte [] id){
+        this.id = id;
     }
 
     @Override
-    public void read(int offset, int size) {
+    public void writeDataToStore(Object baseObj, byte [] bytes, long baseAddress, long offset) {
+        // TODO fileStream write to data
+        try {
+            if(fileOutputStream == null){
+                fileOutputStream = new FileOutputStream(new String(id));
+            }
+            fileOutputStream.write(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Override
+    public void read(byte [] bytes, int offset, int size) {
+        try {
+            if(fileInputStream == null){
+                fileInputStream = new FileInputStream(new String(id));
+            }
+            fileInputStream.read(bytes, offset, size);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void release() {
+        if(fileInputStream != null){
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if(fileOutputStream != null){
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
